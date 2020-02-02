2bb3cdc0532482beed357103efe4e6cbe506ea4a
Vasilis Georgitzikis <tzikis@gmail.com>
2020-02-02 06:41:56 +0200

    Added Game Over Menu

---
+++ Assets/GameoverMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverMenuManager : MonoBehaviour {
+
+	public void ReplayGame(){
+		SceneManager.LoadScene("Main Scene");
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene("Main Menu");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
